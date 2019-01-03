1be0ebb6d99bf4b53d8277ef0dc26f29600af036
evandrocoan <evandrocoan@hotmail.com>
2019-01-03 13:31:44 -0200

    Allowed to run unit tests on python 2.7 with python -m tests
    without installing the package.

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,7 +1,17 @@
 from __future__ import absolute_import, print_function
 
+import os
+import sys
 import unittest
 import logging
+
+def assert_path(module):
+    module = os.path.abspath( module )
+
+    if module not in sys.path:
+        sys.path.insert( 0, module )
+
+assert_path( os.path.join( os.path.basename( os.path.realpath( __file__ ) ), '..', 'source' ) )
 
 from .test_trees import TestTrees
 from .test_tools import TestStandalone
